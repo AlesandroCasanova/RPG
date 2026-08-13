4c7b68c169e907fb745b3213ec2b76db8c36644b
AlesandroCasanova <alesandrocasanova0112@gmail.com>
2026-08-13 03:03:59 -0300

    Agregar orientacion idle en 8 direcciones

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -12,20 +12,24 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	var direction := Input.get_vector(
-		"move_left",
-		"move_right",
-		"move_up",
-		"move_down"
-	)
+	# Leemos WASD por separado.
+	var input_x := Input.get_axis("move_left", "move_right")
+	var input_y := Input.get_axis("move_up", "move_down")
 
+	var direction := Vector2(input_x, input_y)
+
+	# Movimiento isométrico.
 	var iso_direction := Vector2(
 		direction.x - direction.y,
 		(direction.x + direction.y) * 0.5
-	).normalized()
+	)
+
+	if iso_direction != Vector2.ZERO:
+		iso_direction = iso_direction.normalized()
 
 	velocity = iso_direction * speed
 
+	# Actualizar hacia dónde mira el personaje.
 	if direction != Vector2.ZERO:
 		_update_facing(direction)
 
@@ -33,15 +37,39 @@ func _physics_process(_delta: float) -> void:
 
 
 func _update_facing(direction: Vector2) -> void:
-	if abs(direction.x) > abs(direction.y):
-		if direction.x > 0:
-			last_facing = "e"
-		else:
-			last_facing = "w"
-	else:
-		if direction.y > 0:
-			last_facing = "s"
-		else:
-			last_facing = "n"
+	var x := direction.x
+	var y := direction.y
+
+	# W + D
+	if x > 0.0 and y < 0.0:
+		last_facing = "e"
+
+	# W + A
+	elif x < 0.0 and y < 0.0:
+		last_facing = "n"
+
+	# S + D
+	elif x > 0.0 and y > 0.0:
+		last_facing = "s"
+
+	# S + A
+	elif x < 0.0 and y > 0.0:
+		last_facing = "w"
+
+	# W
+	elif y < 0.0:
+		last_facing = "ne"
+
+	# S
+	elif y > 0.0:
+		last_facing = "sw"
+
+	# A
+	elif x < 0.0:
+		last_facing = "nw"
+
+	# D
+	elif x > 0.0:
+		last_facing = "se"
 
 	player_animated.play("idle_" + last_facing)
